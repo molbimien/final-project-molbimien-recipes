95822b79c177e3e5d927ccb100a46dc1c1508bb8
molbimien <43146046+molbimien@users.noreply.github.com>
2022-01-22 20:48:06 +0100

    feat(backend): added UserSchema and User

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = Promise
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    unique: true, 
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true, 
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+  }
+})
+
+const User = mongoose.model('User', UserSchema)
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
