dd920d1eac8a6b0703a3b78e937ac4641fc26e9d
molbimien <43146046+molbimien@users.noreply.github.com>
2022-01-22 21:33:31 +0100

    feat(backend): add authorization of user with access token

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// authenticates user with access token
+const authenticateUser = async (req, res, next) => {
+	const accessToken = req.header("Authorization");
+
+	try {
+		const user = await User.findOne({ accessToken });
+		if (user) {
+			next();
+		} else {
+			res.status(401).json({ response: "Please log in", success: false });
+		}
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+};
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
