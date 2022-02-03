87b519e8186adecd0108062e65265b79cb6d4979
molbimien <43146046+molbimien@users.noreply.github.com>
2022-02-03 17:36:23 +0100

    feat(frontend): update recipe component to show more data from backend

--- frontend/src/Recipe.js
+++ frontend/src/Recipe.js
@@ -20,8 +20,35 @@ const Recipe = () => {
 
     return (
         <Container>
-            <h3>{recipe?.response?.name}</h3>
+            <img
+                src={recipe?.response?.image}
+                alt={recipe?.response?.name}
+            />
+            <h1>{recipe?.response?.name}</h1>
             <p>{recipe?.response?.description}</p>
+            <p>{recipe?.response?.recipeCategory}</p>
+            <h2>Ingredienser</h2>
+            <div>
+            {recipe?.response?.recipeIngredients.map((recipeIngredient, index) => {
+                    return (
+                        <div key={index}>
+                            <p>{recipeIngredient.amount} {recipeIngredient.unit} {recipeIngredient.name}</p>
+                        </div>
+                    )
+                })
+            }
+            </div>
+            <h2>Gör så här</h2>
+            <div>
+            {recipe?.response?.recipeInstruction.map((recipeInstruction, index) => {
+                    return (
+                        <div key={index}>
+                            <p>{recipeInstruction}</p>
+                        </div>
+                    )
+                })
+            }
+            </div>
         </Container>
     )
 }
